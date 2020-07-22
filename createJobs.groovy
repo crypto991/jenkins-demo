efa8cc166fb269057b250239c12d0323bedff0b9
djordje <djordje190@gmail.com>
2020-07-22 13:32:36 +0200

    init

---
+++ createJobs.groovy
@@ -0,0 +1,22 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/crypto991/theme-park-app.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
